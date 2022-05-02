80416ae936024f0658dcad67989259c3e33c015b
abhuse <55142851+abhuse@users.noreply.github.com>
2022-05-02 17:08:23 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# polyloss-pytorch
-Polyloss Pytorch Implementation
+# PolyLoss in Pytorch
+
+PolyLoss implementation in Pytorch as described in:  
+[[Leng et al. 2022] PolyLoss: A Polynomial Expansion Perspective of Classification Loss Functions](https://arxiv.org/abs/2204.12511)
+
+Both Poly-Cross-Entropy and Poly-Focal losses are provided.
+
+## Examples
+
+```python
+import torch
+
+# Poly1 Cross-Entropy Loss
+# classification task
+batch_size = 10
+num_classes = 5
+logits = torch.rand([batch_size, num_classes])
+labels = torch.randint(0, num_classes, [batch_size])
+loss = Poly1CrossEntropyLoss(num_classes=num_classes, 
+                             reduction='mean')
+out = loss(logits, labels)
+out.backward()
+# optimizer.step()
+
+# Poly1 Focal Loss
+# segmentation task
+H, W = 4, 7
+logits = torch.rand([batch_size, num_classes, H, W])
+labels = torch.randint(0, num_classes, [batch_size, H, W])
+loss = Poly1FocalLoss(num_classes=num_classes, 
+                      reduction='mean')
+out = loss(logits, labels)
+out.backward()
+# optimizer.step()
+```
+
+## Parameters
+
+
+### Poly1CrossEntropyLoss
+* ***num_classes***, *(int)* - Number of classes
+* ***epsilon***, *(float)*, *(Default=1.0)* - PolyLoss epsilon
+* ***reduction***, *(str)*, *(Default='none')*  - apply reduction to the output, one of: none | sum | mean.
+
+### Poly1FocalLoss
+* ***num_classes***, *(int)* - Number of classes
+* ***epsilon***, *(float)*, *(Default=1.0)* - PolyLoss epsilon
+* ***alpha***, *(float)*, *(Default=0.25)* - Focal loss alpha 
+* ***gamma***, *(float)*, *(Default=2.0)* - Focal loss gamma
+* ***reduction***, *(str)*, *(Default='none')*  - apply reduction to the output, one of: none | sum | mean.
+
+## Requirements
+* Python 3.6+
+* Pytorch 1.1+
---
+++ polyloss.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Poly1CrossEntropyLoss(nn.Module):
+    def __init__(self,
+                 num_classes: int,
+                 epsilon: float = 1.0,
+                 reduction: str = "none"):
+        """
+        Create instance of Poly1CrossEntropyLoss
+        :param num_classes:
+        :param epsilon:
+        :param reduction: one of none|sum|mean, apply reduction to final loss tensor
+        """
+        super(Poly1CrossEntropyLoss, self).__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+        self.reduction = reduction
+        return
+
+    def forward(self, logits, labels):
+        """
+        Forward pass
+        :param logits: tensor of shape [N, num_classes]
+        :param labels: tensor of shape [N]
+        :return: poly cross-entropy loss
+        """
+        labels_onehot = F.one_hot(labels, num_classes=self.num_classes).to(device=logits.device,
+                                                                           dtype=logits.dtype)
+        pt = torch.sum(labels_onehot * F.softmax(logits, dim=-1), dim=-1)
+        CE = F.cross_entropy(input=logits, target=labels, reduction='none')
+        poly1 = CE + self.epsilon * (1 - pt)
+        if self.reduction == "mean":
+            poly1 = poly1.mean()
+        elif self.reduction == "sum":
+            poly1 = poly1.sum()
+        return poly1
+
+
+class Poly1FocalLoss(nn.Module):
+    def __init__(self,
+                 num_classes: int,
+                 epsilon: float = 1.0,
+                 alpha: float = 0.25,
+                 gamma: float = 2.0,
+                 reduction: str = "none"):
+        """
+        Create instance of Poly1FocalLoss
+        :param num_classes: number of classes
+        :param epsilon: poly loss epsilon
+        :param alpha: focal loss alpha
+        :param gamma: focal loss gamma
+        :param reduction: one of none|sum|mean, apply reduction to final loss tensor
+        """
+        super(Poly1FocalLoss, self).__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+        return
+
+    def forward(self, logits, labels):
+        """
+        Forward pass
+        :param logits: output of neural netwrok of shape [N, num_classes] or [N, num_classes, ...]
+        :param labels: ground truth of shape [N] or [N, ...], NOT one-hot encoded
+        :return: poly focal loss
+        """
+        # focal loss implementation taken from
+        # https://github.com/facebookresearch/fvcore/blob/main/fvcore/nn/focal_loss.py
+
+        p = torch.sigmoid(logits)
+
+        # if labels are of shape [N]
+        # convert to one-hot tensor of shape [N, num_classes]
+        if labels.ndim == 1:
+            labels = F.one_hot(labels, num_classes=self.num_classes)
+
+        # if labels are of shape [N, ...] e.g. segmentation task
+        # convert to one-hot tensor of shape [N, num_classes, ...]
+        else:
+            labels = F.one_hot(labels.unsqueeze(1), self.num_classes).transpose(1, -1).squeeze_(-1)
+
+        labels = labels.to(device=logits.device,
+                           dtype=logits.dtype)
+
+        ce_loss = F.binary_cross_entropy_with_logits(logits, labels, reduction="none")
+        pt = labels * p + (1 - labels) * (1 - p)
+        FL = ce_loss * ((1 - pt) ** self.gamma)
+
+        if self.alpha >= 0:
+            alpha_t = self.alpha * labels + (1 - self.alpha) * (1 - labels)
+            FL = alpha_t * FL
+
+        poly1 = FL + self.epsilon * torch.pow(1 - pt, self.gamma + 1)
+
+        if self.reduction == "mean":
+            poly1 = poly1.mean()
+        elif self.reduction == "sum":
+            poly1 = poly1.sum()
+
+        return poly1
