850561df95470d8b1bacb44e4e810122e7f6f6aa
abhuse <55142851+abhuse@users.noreply.github.com>
2022-05-07 15:20:03 +0200

    weights, onehot

--- polyloss.py
+++ polyloss.py
@@ -1,23 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import Tensor
 
 
 class Poly1CrossEntropyLoss(nn.Module):
     def __init__(self,
                  num_classes: int,
                  epsilon: float = 1.0,
-                 reduction: str = "none"):
+                 reduction: str = "none",
+                 weight: Tensor = None):
         """
         Create instance of Poly1CrossEntropyLoss
         :param num_classes:
         :param epsilon:
         :param reduction: one of none|sum|mean, apply reduction to final loss tensor
+        :param weight: manual rescaling weight for each class, passed to Cross-Entropy loss
         """
         super(Poly1CrossEntropyLoss, self).__init__()
         self.num_classes = num_classes
         self.epsilon = epsilon
         self.reduction = reduction
+        self.weight = weight
         return
 
     def forward(self, logits, labels):
@@ -30,7 +34,10 @@ class Poly1CrossEntropyLoss(nn.Module):
         labels_onehot = F.one_hot(labels, num_classes=self.num_classes).to(device=logits.device,
                                                                            dtype=logits.dtype)
         pt = torch.sum(labels_onehot * F.softmax(logits, dim=-1), dim=-1)
-        CE = F.cross_entropy(input=logits, target=labels, reduction='none')
+        CE = F.cross_entropy(input=logits,
+                             target=labels,
+                             reduction='none',
+                             weight=self.weight)
         poly1 = CE + self.epsilon * (1 - pt)
         if self.reduction == "mean":
             poly1 = poly1.mean()
@@ -45,7 +52,9 @@ class Poly1FocalLoss(nn.Module):
                  epsilon: float = 1.0,
                  alpha: float = 0.25,
                  gamma: float = 2.0,
-                 reduction: str = "none"):
+                 reduction: str = "none",
+                 weight: Tensor = None,
+                 label_is_onehot: bool = False):
         """
         Create instance of Poly1FocalLoss
         :param num_classes: number of classes
@@ -53,6 +62,8 @@ class Poly1FocalLoss(nn.Module):
         :param alpha: focal loss alpha
         :param gamma: focal loss gamma
         :param reduction: one of none|sum|mean, apply reduction to final loss tensor
+        :param weight: manual rescaling weight for each class, passed to binary Cross-Entropy loss
+        :param label_is_onehot: set to True if labels are one-hot encoded
         """
         super(Poly1FocalLoss, self).__init__()
         self.num_classes = num_classes
@@ -60,13 +71,16 @@ class Poly1FocalLoss(nn.Module):
         self.alpha = alpha
         self.gamma = gamma
         self.reduction = reduction
+        self.weight = weight
+        self.label_is_onehot = label_is_onehot
         return
 
     def forward(self, logits, labels):
         """
         Forward pass
         :param logits: output of neural netwrok of shape [N, num_classes] or [N, num_classes, ...]
-        :param labels: ground truth of shape [N] or [N, ...], NOT one-hot encoded
+        :param labels: ground truth tensor of shape [N] or [N, ...] with class ids if label_is_onehot was set to False, otherwise 
+            one-hot encoded tensor of same shape as logits
         :return: poly focal loss
         """
         # focal loss implementation taken from
@@ -74,20 +88,25 @@ class Poly1FocalLoss(nn.Module):
 
         p = torch.sigmoid(logits)
 
-        # if labels are of shape [N]
-        # convert to one-hot tensor of shape [N, num_classes]
-        if labels.ndim == 1:
-            labels = F.one_hot(labels, num_classes=self.num_classes)
+        if not self.label_is_onehot:
+            # if labels are of shape [N]
+            # convert to one-hot tensor of shape [N, num_classes]
+            if labels.ndim == 1:
+                labels = F.one_hot(labels, num_classes=self.num_classes)
 
-        # if labels are of shape [N, ...] e.g. segmentation task
-        # convert to one-hot tensor of shape [N, num_classes, ...]
-        else:
-            labels = F.one_hot(labels.unsqueeze(1), self.num_classes).transpose(1, -1).squeeze_(-1)
+            # if labels are of shape [N, ...] e.g. segmentation task
+            # convert to one-hot tensor of shape [N, num_classes, ...]
+            else:
+                labels = F.one_hot(labels.unsqueeze(
+                    1), self.num_classes).transpose(1, -1).squeeze_(-1)
 
         labels = labels.to(device=logits.device,
                            dtype=logits.dtype)
 
-        ce_loss = F.binary_cross_entropy_with_logits(logits, labels, reduction="none")
+        ce_loss = F.binary_cross_entropy_with_logits(input=logits,
+                                                     target=labels,
+                                                     reduction="none",
+                                                     weight=self.weight)
         pt = labels * p + (1 - labels) * (1 - p)
         FL = ce_loss * ((1 - pt) ** self.gamma)
 
